3fb56e1c3301807772a5219bad6696e8cda4347f
Daniel Molkentin <daniel@molkentin.de>
2022-11-30 19:14:05 +0100

    fix: fetch release branches before trying to access them

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+
+git branch -r | grep '/v' | while read -r version; do
+  branch=${version##*/}
+  remote=${version%/*}
+  git fetch $remote $branch:$branch
+done
 
 git branch | sed -n 's/^.*v\(.*\).x-release/\1/p' \
-  | while read version; do
+  | while read -r version; do
 
   echo "Adding documentation for $version"
   git checkout "v${version}.x-release"
